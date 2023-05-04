2e4ea3fd31611d8a485dc25d47d9c1c3a2918e22
vaishnavi-g06 <131155303+vaishnavi-g06@users.noreply.github.com>
2023-05-04 18:53:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZZFSRYKPC53PPB7I"
-  secret_key = "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"
+  access_key = "AKII"
+  secret_key = "CMHC"
 }
